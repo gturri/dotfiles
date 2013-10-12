e9535f007f85f7a753b2bb68d5116fb37cbae13e
Guillaume Turri <guillaume.turri@gmail.com>
2013-10-12 13:04:42 +0200

    Remove Mac homebrew section in bootstrap
    This code was copy/pasted from elsewhere, and I can't maintain it

--- bootstrap.sh
+++ bootstrap.sh
@@ -207,15 +207,5 @@ setup_vimrc
 setup_bashrc
 install_dotfiles
 
-# If we are on a mac, lets install and setup homebrew
-if [ "$(uname -s)" == "Darwin" ]; then
-  info "installing dependencies"
-  if . bin/dot > /tmp/dotfiles-dot 2>&1; then
-    success "dependencies installed"
-  else
-    fail "error installing dependencies"
-  fi
-fi
-
 echo ''
 echo ' All installed!'
