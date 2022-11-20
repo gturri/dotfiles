ab8140704b5a163a421134f1461727d3f817fcf0
Guillaume Turri <guillaume.turri@gmail.com>
2022-11-20 13:03:57 +0100

    Better handling of invalid yaml

--- src/main/java/fr/turri/YamlToJson.java
+++ src/main/java/fr/turri/YamlToJson.java
@@ -2,12 +2,18 @@ package fr.turri;
 
 import com.google.gson.GsonBuilder;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.parser.ParserException;
 
 import java.util.Map;
 
 public class YamlToJson {
     public static void main(String[] args) {
-        Map<String, Object> parsedYaml = new Yaml().load(System.in);
-        System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(parsedYaml));
+        try {
+            Map<String, Object> parsedYaml = new Yaml().load(System.in);
+            System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(parsedYaml));
+        } catch (ParserException e) {
+            System.err.println("Failed to parse the yaml document: " + e.getMessage());
+            System.exit(1);
+        }
     }
 }
