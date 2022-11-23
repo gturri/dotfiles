66bbbf450664204f6822eb9b8481ee37d9080a27
Guillaume Turri <g.turri@criteo.com>
2022-11-23 15:49:03 +0100

    Add flags to EpochToDate

--- src/main/java/fr/turri/EpochToDate.java
+++ src/main/java/fr/turri/EpochToDate.java
@@ -1,5 +1,6 @@
 package fr.turri;
 
+import com.martiansoftware.jsap.*;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -9,13 +10,52 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class EpochToDate {
+    private static final String LOCAL_TIME_OPTION = "localTime";
+
     public static void main(String[] args) throws IOException {
+        JSAPResult config = handleCommandLine(args);
+
         long epoch = Long.parseLong(IOUtils.toString(System.in, StandardCharsets.UTF_8).trim());
         Date date = new Date(epoch * 1000);
 
         SimpleDateFormat sdf;
-        sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        if (config.getBoolean(LOCAL_TIME_OPTION)) {
+            sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        } else {
+            sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        }
         System.out.println(sdf.format(date));
+    }
+
+    private static JSAPResult handleCommandLine(String[] args) {
+        JSAP jsap = new JSAP();
+
+        try {
+            jsap.registerParameter(new Switch("help")
+                    .setLongFlag("help")
+                    .setShortFlag('h'));
+            jsap.registerParameter(new Switch(LOCAL_TIME_OPTION)
+                    .setLongFlag("localtime")
+                    .setShortFlag('l'));
+        } catch(JSAPException e) {
+            throw new RuntimeException("Something went really wrong", e);
+        }
+
+        JSAPResult config = jsap.parse(args);
+
+        if (!config.success()) {
+            for(@SuppressWarnings("rawtypes") java.util.Iterator errs = config.getErrorMessageIterator() ; errs.hasNext();) {
+                System.err.println(errs.next());
+            }
+            System.err.println(jsap.getUsage());
+            System.exit(1);
+        }
+        if (config.getBoolean("help")) {
+            System.out.println(jsap.getUsage());
+            System.exit(0);
+        }
+
+        return config;
     }
 }
