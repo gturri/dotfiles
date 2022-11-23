0fa6f6346da306db63bd6f42bfae30fddaac6a5d
Guillaume Turri <g.turri@criteo.com>
2022-11-23 15:44:13 +0100

    Trim input string in DateToEpoch
    to avoid parsing failure due e.g. to trailing whitespaces

--- src/main/java/fr/turri/DateToEpoch.java
+++ src/main/java/fr/turri/DateToEpoch.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 public class DateToEpoch {
     public static void main(String[] args) throws IOException {
-        Date date = Iso8601Deserializer.toDate(IOUtils.toString(System.in, StandardCharsets.UTF_8));
+        Date date = Iso8601Deserializer.toDate(IOUtils.toString(System.in, StandardCharsets.UTF_8).trim());
         System.out.println(date.getTime() / 1000);
     }
 }
